cd9ab14e49acbf9cb822f7fbe2e405f90fd1b3ea
STORKDev <jufoli@gmail.com>
2023-03-30 02:44:47 +0300

    Base functionality

--- monaISPip/Form1.Designer.cs
+++ monaISPip/Form1.Designer.cs
@@ -24,13 +24,76 @@ namespace monaISPip {
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components=new System.ComponentModel.Container();
-            this.AutoScaleMode=System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize=new System.Drawing.Size(800,450);
-            this.Text="Form1";
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.curip = new System.Windows.Forms.Label();
+            this.curreg = new System.Windows.Forms.Label();
+            this.curisp = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 360000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // curip
+            // 
+            this.curip.AutoSize = true;
+            this.curip.Font = new System.Drawing.Font("Segoe UI Emoji", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.curip.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.curip.Location = new System.Drawing.Point(44, 9);
+            this.curip.Name = "curip";
+            this.curip.Size = new System.Drawing.Size(125, 37);
+            this.curip.TabIndex = 0;
+            this.curip.Text = "127.0.0.1";
+            // 
+            // curreg
+            // 
+            this.curreg.AutoSize = true;
+            this.curreg.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.curreg.Location = new System.Drawing.Point(44, 46);
+            this.curreg.Name = "curreg";
+            this.curreg.Size = new System.Drawing.Size(153, 28);
+            this.curreg.TabIndex = 1;
+            this.curreg.Text = "Current location";
+            // 
+            // curisp
+            // 
+            this.curisp.AutoSize = true;
+            this.curisp.Location = new System.Drawing.Point(44, 84);
+            this.curisp.Name = "curisp";
+            this.curisp.Size = new System.Drawing.Size(94, 20);
+            this.curisp.TabIndex = 2;
+            this.curisp.Text = "Unknown ISP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 128);
+            this.Controls.Add(this.curisp);
+            this.Controls.Add(this.curreg);
+            this.Controls.Add(this.curip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "monaISPip";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label curip;
+        private System.Windows.Forms.Label curreg;
+        private System.Windows.Forms.Label curisp;
     }
 }
 
--- monaISPip/Form1.cs
+++ monaISPip/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json;
+
 
 namespace monaISPip {
     public partial class Form1:Form {
@@ -14,5 +17,50 @@ namespace monaISPip {
             InitializeComponent();
         }
 
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd,int Msg,int wParam,int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Form1_MouseDown(object sender,System.Windows.Forms.MouseEventArgs e) {
+            if(e.Button==MouseButtons.Left) {
+                ReleaseCapture();
+                SendMessage(Handle,WM_NCLBUTTONDOWN,HT_CAPTION,0);
+            }
+        }
+
+        private void Form1_Load(object sender,EventArgs e) {
+            BackColor=Color.Lime;
+            TransparencyKey=Color.Lime;
+            this.Icon=Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+            timer1_Tick(sender,e); 
+            this.TopMost=true;
+            this.Show();
+        }
+
+        private void Form1_MouseClick(object sender,MouseEventArgs e) {
+
+        }
+
+        private async void timer1_Tick(object sender,EventArgs e) {
+            try {
+
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.GetAsync("http://ip-api.com/json/");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                dynamic api_data_js = JsonConvert.DeserializeObject(responseBody);
+
+                curip.Text=api_data_js.query;
+                curreg.Text=api_data_js.city+", "+api_data_js.country;
+                curisp.Text=api_data_js["as"] +" ("+ api_data_js.isp +")";
+
+            } catch (Exception err){
+                return;
+            }
+        }
     }
 }
